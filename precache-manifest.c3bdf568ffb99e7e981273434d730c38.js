93abce5219af520c8f596d34748813a8e31ab6fc
Darsh Jain <Darsh@Pujas-MacBook-Air.local>
2020-11-11 18:26:07 -0500

    Updates

---
+++ precache-manifest.c3bdf568ffb99e7e981273434d730c38.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1f968cad739c38b5445b10e7e317389",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a29cfede5c65e45259db",
+    "url": "./static/js/2.1f2b7f6d.chunk.js"
+  },
+  {
+    "revision": "89bfb9a1a8ff2772928c",
+    "url": "./static/js/main.5311bc34.chunk.js"
+  },
+  {
+    "revision": "7c41025e2cf651752eb0",
+    "url": "./static/js/runtime-main.536a80b0.js"
+  }
+]);
